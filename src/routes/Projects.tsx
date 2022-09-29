95531a0dd3c1359d154dc87339bb95efed5654aa
Lui H <tech.luiholl@gmail.com>
2022-09-29 15:05:16 +0200

    Passed headers and text as props to project comp

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+// ==== Components =====
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
+import ProjectsComp from "../components/Projects/ProjectsComp";
 
 const Projects = (): JSX.Element => {
   return (
     <div>
       <NavBar/>
+      <ProjectsComp 
+        heading="Projects" 
+        text="a few things I've been tinkering on.."
+      />
       <Footer/>
     </div>
   )
