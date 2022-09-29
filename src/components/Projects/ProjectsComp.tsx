5bfdf798e5263210778ee9d6d95f57c11c3f6ec6
Lui H <tech.luiholl@gmail.com>
2022-09-29 15:05:03 +0200

    Project component

---
+++ src/components/Projects/ProjectsComp.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import "./ProjectsComp.css"
+
+interface ProjectProps {
+  heading: string
+  text: string
+}
+
+const ProjectsComp = ({heading, text}: ProjectProps): JSX.Element => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>{heading}</h1>
+        <p>{text}</p>
+      </div>
+    </div>
+  )
+};
+
+export default ProjectsComp;
