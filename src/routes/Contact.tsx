5574bea8495694e534d86d019a9b254e8847173b
Lui H <tech.luiholl@gmail.com>
2022-10-03 14:33:22 +0100

    Contact me import

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 import ProjectsComp from "../components/Projects/ProjectsComp";
+import ContactMe from "../components/Contact/ContactMe";
 
 const Contact = (): JSX.Element => {
   return (
     <div>
       <NavBar />
       <ProjectsComp heading="Contact" text="Let's talk!" />
+      <ContactMe />
       <Footer />
     </div>
   );
