5e6c4fa4877c3684853d14b36dc6389f7d449cf8
Lui H <tech.luiholl@gmail.com>
2022-10-03 14:32:03 +0100

    contact me page

---
+++ src/components/Contact/ContactMe.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import "./ContactMe.css"
+
+const ContactMe = (): JSX.Element => {
+  return (
+    <div className="form">
+      <form action="https://public.herotofu.com/v1/6fa96b40-431d-11ed-9b17-6fdf7f94f506" method="post">
+        <label htmlFor="name">Your name</label>
+        <input name="Name" id="name" type="text" placeholder="required..."></input>
+        <label htmlFor="email">Email</label>
+        <input name="Email" id="email" type="email" placeholder="required..."></input>
+        <label>Subject</label>
+        <input name="Subject" id="subject" type="text" placeholder="optional..."></input>
+        <label>Message</label>
+        <textarea name="Message" id="message" rows={6} placeholder="type your message here" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+};
+
+/*
+<form action="https://public.herotofu.com/v1/6fa96b40-431d-11ed-9b17-6fdf7f94f506" method="post">
+  <div>
+    <label for="name">Your Name</label>
+    <input name="Name" id="name" type="text" required />
+  </div>
+  <div>
+    <label for="email">Your Email</label>
+    <input name="Email" id="email" type="email" required  />
+  </div>
+  <div>
+    <input type="submit" value="Download CTA" />
+  </div>
+</form>
+*/
+
+export default ContactMe;
